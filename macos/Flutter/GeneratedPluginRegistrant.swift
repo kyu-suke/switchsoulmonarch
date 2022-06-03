a2191246203197e9a913bec10ea9e68e8d05321f
kyu-suke <ykaneoka7@gmail.com>
2022-06-03 12:16:50 +0900

    ⬆️: packages

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import hotkey_holder
 import magnetica
+import screen_retriever
 import sqflite
 import system_tray
 import tray_manager
@@ -15,6 +16,7 @@ import window_manager
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   HotkeyHolderPlugin.register(with: registry.registrar(forPlugin: "HotkeyHolderPlugin"))
   MagneticaPlugin.register(with: registry.registrar(forPlugin: "MagneticaPlugin"))
+  ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   SystemTrayPlugin.register(with: registry.registrar(forPlugin: "SystemTrayPlugin"))
   TrayManagerPlugin.register(with: registry.registrar(forPlugin: "TrayManagerPlugin"))
